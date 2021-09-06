20188469db5169f08ff59de0b8154b325cf04062
yutatsune <66116053+yutatsune@users.noreply.github.com>
2021-09-06 23:59:59 +0900

    新規投稿機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,8 +3,17 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.order(id: :DESC).page(params[:page]).per(PER_PAGE)
+    @post = Post.new
   end
 
   def create
+    post = Post.create!(post_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body)
   end
 end
